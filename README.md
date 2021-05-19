e434aab9ac9055a0e91b594141d63ee7b8bc4a95
Thi Hieu <thihieuluu@gmail.com>
2021-05-19 07:28:33 +0200

    First version with signup, login

---
+++ README.md
@@ -0,0 +1,29 @@
+# Project 10
+## About the project 10
+This project is realized with Django REST framework in order to create an API for tracking projects.
+
+[comment]: <> (The main goal of this application is to:)
+
+[comment]: <> (* Allow users to post theirs requests of reviews about a book or theirs reviews. )
+
+[comment]: <> (* Follow the other users.)
+## About main structure
+* Project: softdesk_project
+* Application: users
+* Application:  project_tracking_app
+## Code organization
+## Process
+1. Clone and launch the project:
+```
+git clone  https://github.com/ThiHieuLUU/OCProject10.git
+cd OCProject10/
+
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt 
+
+cd softdesk_project/
+
+python manage.py runserver
+```
+Then go to http://127.0.0.1:8000/.
---
+++ softdesk_project/users/models.py
@@ -0,0 +1,84 @@
+"""Custom User model."""
+
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+
+
+class CustomUserManager(BaseUserManager):
+    """User manager model."""
+
+    def create_superuser(self, email, first_name, last_name, password, **other_fields):
+
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(email, first_name, last_name, password, **other_fields)
+
+    def create_user(self, email, first_name, last_name, password, **other_fields):
+
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email,
+                          first_name=first_name, last_name=last_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User model."""
+
+    # These fields tie to the roles!
+    AUTHOR = 1
+    MANAGER = 2
+    CREATOR = 3
+
+    ROLE_CHOICES = (
+        (AUTHOR, 'Author'),
+        (MANAGER, 'Manager'),
+        (CREATOR, 'Creator')
+    )
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    uid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public identifier')
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True, default=7)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_staff = models.BooleanField(default=False)  # Must have this field
+    # is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+    created_by = models.EmailField()
+    modified_by = models.EmailField()
+
+    # REQUIRED_FIELDS contains all required fields on your user model,
+    # except the username field and password, as these fields will always be prompted for.
+    USERNAME_FIELD = 'email'  # user_name will be displayed by default, replace here by email
+    REQUIRED_FIELDS = ['first_name', 'last_name']  # Fields will be displayed : first/last_name, email, password
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
