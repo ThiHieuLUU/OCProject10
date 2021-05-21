e110b6f5a23bf171ab99feeb70d64a52b7d7d719
Thi Hieu <thihieuluu@gmail.com>
2021-05-21 09:56:55 +0200

    Test nested router without relationship models

--- softdesk_project/project_tracking_app/urls.py
+++ softdesk_project/project_tracking_app/urls.py
@@ -15,20 +15,18 @@ router.register(r'projects', ProjectViewSet)
 # Generate: /viewset/projects/{project_pk}
 
 projects_router = routers.NestedSimpleRouter(router, r'projects', lookup='project')
-projects_router.register(r'comments', CommentViewSet, basename='comments')
-# Generate: /viewset/projects/{project_pk}/comments/
-# Generate: /viewset/projects/{project_pk}/comments/{comment_pk}
+projects_router.register(r'issues', IssueViewSet, basename='issues')
+# Generate: /viewset/projects/{project_pk}/issues/
+# Generate: /viewset/projects/{project_pk}/issues/{issue_pk}
 
-
-comments_router = routers.NestedSimpleRouter(projects_router, r'comments', lookup='comment')
-comments_router.register(r'issues', IssueViewSet, basename='issues')
-# Generate: /viewset/projects/{project_pk}/comments/issues/
-# Generate: /viewset/projects/{project_pk}/comments/{comment_pk}/issues/{issue_pk}
-
+issues_router = routers.NestedSimpleRouter(projects_router, r'issues', lookup='issue')
+issues_router.register(r'comments', CommentViewSet, basename='comments')
+# Generate: /viewset/projects/{project_pk}/issues/{issue_pk}/comments/
+# Generate: /viewset/projects/{project_pk}/issues/{issue_pk}/comments/{comment_pk}
 
 # http://127.0.0.1:8000/viewset/projects/3/comments/1/issues/1/
 urlpatterns = [
     path('viewset/', include(router.urls)),
     path('viewset/', include(projects_router.urls)),
-    path('viewset/', include(comments_router.urls)),
+    path('viewset/', include(issues_router.urls)),
 ]
